1ba5d3d79894c2671052b2b7afc9329a98759414
TheBlob42 <hessenmobbel@web.de>
2022-05-06 08:36:54 +0200

    refactor: new nvim-lsp-installer setup

--- lua/lsp/lsp-installer.lua
+++ lua/lsp/lsp-installer.lua
@@ -1,30 +1,46 @@
-local status_ok, lsp_installer = my.req('nvim-lsp-installer')
+local status_ok, lsp_installer, servers, lspconfig = my.req('nvim-lsp-installer', 'nvim-lsp-installer.servers', 'lspconfig')
 if not status_ok then
     return
 end
 
-lsp_installer.on_server_ready(function(server)
-    local config = {
+lsp_installer.setup()
+
+-- set lspconfig default server configuration
+lspconfig.util.default_config = vim.tbl_extend(
+    'force',
+    lspconfig.util.default_config,
+    {
         on_attach = require('lsp.handlers').on_attach,
         capabilities = require('lsp.handlers').make_capabilities(),
     }
+)
 
-    if server.name == 'jdtls' then
-        -- special handling for java (see 'ftplugin/java.lua')
-        return
+-- setup all servers installed via 'nvim-lsp-installer'
+for _, server in ipairs(servers.get_installed_servers()) do
+    local config = {}
+    local name = server.name
+
+    if name == 'jdtls' then
+        goto continue -- special handling for java (see `ftplugin/java.lua`)
     end
 
-    if server.name == 'sumneko_lua' then
+    if name == 'sumneko_lua' then
         config = require('lsp.servers.sumneko_lua').config
     end
 
-    if server.name == 'jsonls' then
-        config = require('lsp.servers.jsonls').config
-    end
-
-    if server.name == 'tsserver' then
+    if name == 'tsserver' then
         config = require('lsp.servers.tsserver').config
     end
 
-    server:setup(config)
-end)
+    if name == 'jsonls' then
+        config = {
+            filetypes = { 'json', 'jsonc' },
+        }
+    end
+
+    lspconfig[name].setup(config)
+    ::continue::
+end
+
+-- unfortunately there is currently no "hook" to directly setup a new server after it has been installed
+-- this might get fixed in the future, for now we have to call ':lua require"lspconfig".<server>.setup()'
--- lua/lsp/servers/jsonls.lua
+++
@@ -1,7 +0,0 @@
-return {
-    config = {
-        on_attach = require('lsp.handlers').on_attach,
-        capabilities = require('lsp.handlers').make_capabilities(),
-        filetypes = { 'json', 'jsonc' }
-    }
-}
