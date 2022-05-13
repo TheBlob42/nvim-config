ac2a3a95156434c7e9ccafd072e5b561ceee36cf
TheBlob42 <hessenmobbel@web.de>
2022-05-13 17:49:47 +0200

    fix: delete unwanted snippet files

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -29,6 +29,10 @@ luasnip.add_snippets('markdown', require('plugins.snippets.markdown'))
 luasnip.add_snippets('java', require('plugins.snippets.java'))
 luasnip.add_snippets('lua', require('plugins.snippets.lua'))
 
--- avoid duplicate snippets for certain file types by deleting
--- the corresponding snippet files from the friendly snippets plugin
+-- avoid duplicate & unwanted snippets by deleting snippet files from friendly snippet (but keep lazy loading functionality)
+local repo_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/friendly-snippets/snippets/'
+for _, ft in ipairs({ 'lua', 'markdown' }) do
+    os.remove(repo_path .. ft .. '.json')
+end
+
 require("luasnip/loaders/from_vscode").lazy_load()
