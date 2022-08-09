624e6245e0bb5b608d99f0c6021154ffb7cd6824
TheBlob42 <hessenmobbel@web.de>
2022-08-09 14:32:13 +0200

    feat: make JsonFormat work on ranges

--- lua/user/commands/json.lua
+++ lua/user/commands/json.lua
@@ -1,17 +1,20 @@
--- try to format the current buffer as JSON
-vim.api.nvim_create_user_command('JsonFormat', function()
+-- try to format the current range (default: whole buffer) as JSON
+vim.api.nvim_create_user_command('JsonFormat', function(args)
     if vim.fn.executable('jq') == 0 then
         vim.api.nvim_echo({{ "'jq' was not found in PATH, install it in order to use this command!" , 'ErrorMsg'}}, false, {})
         return
     end
 
-    -- check for JSON or scratch buffer
-    local ft = vim.api.nvim_buf_get_option(0, 'filetype')
-    if ft == '' or ft == 'json' then
+    local first = args.line1
+    local last = args.line2
+
+    -- if the whole buffer is JSON we can set the appropriate filetype
+    if first == 1 and last == vim.fn.line('$') then
         vim.api.nvim_buf_set_option(0, 'filetype', 'json')
-        vim.api.nvim_command('%!jq .')
-        return
     end
 
-    print("Not a JSON buffer, please check 'filetype'!")
-end, { desc = 'format json' })
+    vim.cmd(string.format('%s,%s!jq .', first, last))
+end, {
+    desc = 'format json',
+    range = '%',
+})
