5f1ddddf5d6144cbd9ce5aa74aa4555d27407c78
TheBlob42 <hessenmobbel@web.de>
2022-08-31 13:57:32 +0200

    refactor: replace custom keybindings function
    I never used it, but to have a VERY similar option at hand I just mapped
    <A-x> to the keymaps picker provided by telescope.nvim

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,11 +6,7 @@ end
 local actions = require('telescope.actions')
 local action_state = require('telescope.actions.state')
 local transform_mod = require('telescope.actions.mt').transform_mod
-local pickers = require('telescope.pickers')
-local finders = require('telescope.finders')
-local conf = require('telescope.config').values
 local builtin = require('telescope.builtin')
-local make_entry = require('telescope.make_entry')
 
 -- ~~~~~~~~~~~~~~~~~~
 -- ~ custom actions ~
@@ -133,61 +129,12 @@ require('telescope').load_extension('project')
 require('telescope').load_extension('file_browser')
 require('telescope').load_extension('fzf')
 
--- ~~~~~~~~~~~~~~~~~~~~
--- ~ custom functions ~
--- ~~~~~~~~~~~~~~~~~~~~
-
-local function my_keymaps()
-    local keymaps_encountered = {}
-    local keymaps_table = {}
-    local max_len_lhs = 0
-
-    local function extract_keymap(keymaps)
-        for _, keymap in pairs(keymaps) do
-            local keymap_key = keymap.buffer .. keymap.mode .. keymap.lhs
-            if not keymaps_encountered[keymap_key] and keymap.desc then
-                keymaps_encountered[keymap_key] = true
-                table.insert(keymaps_table, keymap)
-                max_len_lhs = math.max(max_len_lhs, #require('telescope.utils').display_termcodes(keymap.lhs))
-            end
-        end
-    end
-
-    for _, mode in ipairs({ 'n', 'x', 'i', 'c' }) do
-        local global = vim.api.nvim_get_keymap(mode)
-        local buf_local = vim.api.nvim_buf_get_keymap(0, mode)
-        extract_keymap(global)
-        extract_keymap(buf_local)
-    end
-
-    pickers.new({}, {
-        prompt_title = 'My Key Maps',
-        finder = finders.new_table {
-            results = keymaps_table,
-            entry_maker = make_entry.gen_from_keymaps { width_lhs = max_len_lhs + 1 },
-        },
-        sorter = conf.generic_sorter {},
-        attach_mappings = function(prompt_bufnr)
-            actions.select_default:replace(function()
-                local selection = action_state.get_selected_entry()
-                if not selection then
-                  return
-                end
-
-                vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(selection.value.lhs, true, false, true), 't', true)
-                return actions.close(prompt_bufnr)
-            end)
-            return true
-        end,
-    }):find()
-end
-
 -- ~~~~~~~~~~~~~~~
 -- ~ keymappings ~
 -- ~~~~~~~~~~~~~~~
 
 local mappings = {
-    { '<A-x>',  my_keymaps, 'my keymappings' },
+    { '<A-x>',  '<CMD>Telescope keymaps show_plug=false<CR>', 'keymappings' },
     { '<leader>h',  '<CMD>Telescope help_tags<CR>', 'help tags' },
     { '<leader>bb', '<CMD>Telescope buffers<CR>', 'switch buffers' },
     { '<leader>el', '<CMD>Telescope diagnostics bufnr=0<CR>', 'list errors' },
