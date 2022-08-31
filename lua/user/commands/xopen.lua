d368f36015e48b859addf8c4b133b1668581c716
TheBlob42 <hessenmobbel@web.de>
2022-08-31 13:15:02 +0200

    fix: add quotation marks to xopen regex

--- lua/user/commands/xopen.lua
+++ lua/user/commands/xopen.lua
@@ -1,8 +1,8 @@
 -- open the link under the cursor with the default application (using `xdg-open`)
 vim.api.nvim_create_user_command('XOpen', function()
     local link = vim.fn.expand('<cWORD>')
-    -- perform some "cleanup" (e.g. link within parentheses)
-    link = link:match('[^a-z]*([a-z]+://[^<>,;()]*)[<>,;()]*')
+    -- perform some "cleanup" (e.g. link within parentheses, quotation marks, etc.)
+    link = link:match([[[^a-z]*([a-z]+://[^'"<>,;()]*)]])
     link = vim.fn.shellescape(link, 1)
 
     -- NOTE: xdg-open is only available for Linux
