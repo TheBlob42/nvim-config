125ac132806839a6d04bb3a7ac009f1b65748d17
TheBlob42 <hessenmobbel@web.de>
2022-11-17 11:15:45 +0100

    fix(sumneko): block popup

--- lua/lsp/servers/sumneko_lua.lua
+++ lua/lsp/servers/sumneko_lua.lua
@@ -3,6 +3,14 @@ return {
         lspconfig = {
             on_attach = require('lsp.utils').on_attach,
             capabilities = require('lsp.utils').capabilities,
+            settings = {
+                Lua = {
+                    workspace = {
+                        -- https://github.com/neovim/nvim-lspconfig/issues/1700#issuecomment-1033127328
+                        checkThirdParty = false,
+                    }
+                }
+            }
         }
     }
 }
