7c47326d19fb03283c87ca5eb2216aa2699f1636
TheBlob42 <hessenmobbel@web.de>
2023-03-17 09:17:26 +0100

    feat(fzf): better "save as" feature
    The "save as" keymap was remove in 850ff0b8 by accident
    
    Implement an easier way to save a buffer into a specific file using a
    fzf dialog. Also refactor the fzf class to easier create other custom
    file explorers dialogs

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -64,13 +64,28 @@ fzf.setup {
     },
 }
 
-local function file_explorer(dir)
-    dir = dir or vim.loop.cwd() -- default to the cwd
+---@class FzfFilesOption
+---@field prompt string|function The prompt string for the fzf dialog. Can also be a function which receives the directory path (e.g. '/home/user/projects') and returns the prompt string to use
+---@field transform function Function to transform the displayed entries for the fzf dialog. The function receives the list of entries (strings) to display and returns a new list which is then displayed in the fzf dialog. Use it to add, delete or modify the default display entries for your custom use case
+---@field actions function Function to create the actions for the fzf dialog. The function receives the directory path (e.g. '/home/user/projects') and the entries map (e.g. `{ ['file.txt'] = { type = 'file', path = '/home/user/file.txt' } }`) as parameters, so they can be used for the definition of custom actions. The return is a map of actions as defined by fzf-lua
+
+---Display the files & directories in `dir` (by default the current working directory)
+---By default only allows simple navigation through directories and opening a selected file in the same window
+---Pass a `FzfFilesOption` table to further customization of the fzf dialog
+---@param dir string? The directory to open in fzf
+---@param opts FzfFilesOption? Additional options for customize the fzf dialog
+---@see FzfFilesOption
+local function files(dir, opts)
+    vim.validate {
+        dir = { dir, 'string', true },
+        opts = { opts, 'table', true }
+    }
+
+    dir = assert(dir or vim.loop.cwd())
     dir = vim.fn.fnamemodify(vim.fn.expand(dir), ':p') -- ensure trailing path separator
 
     local entries = {}
-
-    -- "goto parent" entry only if not at root level
+    -- "goto parent" entry only if we're not at root level
     if dir ~= '/' then
         entries[' ..'] = {
             type = 'directory',
@@ -93,62 +108,46 @@ local function file_explorer(dir)
         end
     end
 
-    local edit_file = function(pre_cmd)
-        return function(selected)
-            local element = entries[selected[1]]
-
-            if element.type ~= 'directory' then
-                vim.api.nvim_win_close(0, true)
-                if pre_cmd then
-                    vim.cmd(pre_cmd)
-                end
-                vim.cmd.e(element.path)
-            else
-                fzf.actions.resume()
-            end
-        end
-    end
-
-    local display_dir = vim.fn.fnamemodify(dir, ':~')
     local display_entries = vim.tbl_keys(entries)
     table.sort(display_entries)
 
-    fzf.fzf_exec(display_entries, {
-        prompt = display_dir..'> ',
+    -- "clone" the given options table to prevent modifying the original in case of recursive fn calls
+    local options = vim.tbl_extend('force', {}, opts)
+    if options.actions then
+        -- create the custom actions to make them easier "mergeable" with the defaults
+        options.actions = options.actions(dir, entries)
+    end
+    local defaults_opts = {
+        prompt = vim.fn.fnamemodify(dir, ':~') .. '> ',
+        transform = nil,
         actions = {
             ['default'] = {
                 function(selected)
                     local element = entries[selected[1]]
 
                     if element.type == 'directory' then
-                        file_explorer(element.path)
+                        files(element.path, opts)
                     else
                         vim.api.nvim_win_close(0, true)
                         vim.cmd.e(element.path)
                     end
                 end,
             },
-            ['alt-d'] = function()
-                require('drex').open_directory_buffer(dir)
-            end,
-            ['alt-s'] = {
-                function()
-                    fzf.live_grep {
-                        prompt = 'Search "'..display_dir..'"> ',
-                        cwd = dir,
-                    }
-                end,
-                fzf.actions.resume,
-            },
-            ['alt-t'] = function()
-                local buf = vim.api.nvim_create_buf(true, false)
-                vim.api.nvim_set_current_buf(buf)
-                vim.fn.termopen(vim.o.shell, { cwd = dir })
-            end,
-            ['ctrl-v'] = { edit_file('vsplit') },
-            ['ctrl-x'] = { edit_file('split') },
-            ['ctrl-t'] = { edit_file('tabnew') },
         },
+    }
+    options = vim.tbl_deep_extend('force', defaults_opts, options)
+
+    if type(options.prompt) == 'function' then
+        options.prompt = options.prompt(dir)
+    end
+
+    if options.transform then
+        display_entries = options.transform(display_entries)
+    end
+
+    fzf.fzf_exec(display_entries, {
+        prompt = options.prompt,
+        actions = options.actions,
         winopts = {
             on_create = function()
                 for _, icon in pairs(require('nvim-web-devicons').get_icons()) do
@@ -156,6 +155,105 @@ local function file_explorer(dir)
                 end
             end,
         },
+    })
+end
+
+local function save_as(directory)
+    local create_new = '+ [CREATE NEW FILE]'
+    files(directory, {
+        prompt = function(dir)
+            return 'Save in "'..vim.fn.fnamemodify(dir, ':~')..'"> '
+        end,
+        transform = function(display_entries)
+            table.insert(display_entries, 2, create_new)
+            return display_entries
+        end,
+        actions = function(dir, entries)
+            return {
+                ['default'] = {
+                    function(selected)
+                        if selected[1] == create_new then
+                            vim.api.nvim_win_close(0, true)
+                            local ok, new_file = pcall(vim.fn.input, 'Save as: ', dir)
+                            if not ok then
+                                return
+                            end
+                            vim.cmd.w(new_file)
+                            return
+                        end
+
+                        local element = entries[selected[1]]
+
+                        if element.type == 'directory' then
+                            save_as(element.path)
+                        else
+                            vim.api.nvim_win_close(0, true)
+                            local overwrite = vim.fn.confirm('Do you want to overwrite "' .. element.path .. '"?', '&Yes\n&No', 1, 'Question')
+                            if overwrite == 1 then
+                                local buf = vim.fn.bufnr('^'..element.path..'$')
+                                if buf ~= -1 then
+                                    -- this could cause problems with large files (for now it should be ok)
+                                    vim.fn.writefile(vim.api.nvim_buf_get_lines(0, 0, -1, false), element.path)
+                                    vim.api.nvim_buf_call(buf, function()
+                                        vim.cmd.e()
+                                    end)
+                                else
+                                    vim.cmd.w(element.path)
+                                end
+                            end
+                        end
+                    end
+                },
+            }
+        end,
+    })
+end
+
+local function file_explorer(directory)
+    files(directory, {
+        actions = function(dir, entries)
+            -- a little helper function to avoid repetition
+            local edit_file = function(pre_cmd)
+                return {
+                    function(selected)
+                        local element = entries[selected[1]]
+
+                        if element.type ~= 'directory' then
+                            vim.api.nvim_win_close(0, true)
+                            if pre_cmd then
+                                vim.cmd(pre_cmd)
+                            end
+                            vim.cmd.e(element.path)
+                        else
+                            fzf.actions.resume()
+                        end
+                    end
+                }
+            end
+
+            return {
+                ['alt-d'] = function()
+                    require('drex').open_directory_buffer(dir)
+                end,
+                ['alt-s'] = {
+                    function()
+                        fzf.live_grep {
+                            prompt = 'Search "'..dir..'"> ',
+                            cwd = dir,
+                        }
+                    end,
+                    fzf.actions.resume,
+                },
+                ['alt-t'] = function()
+                    local buf = vim.api.nvim_create_buf(true, false)
+                    vim.api.nvim_set_current_buf(buf)
+                    vim.fn.termopen(vim.o.shell, { cwd = dir })
+                end,
+                ['ctrl-v'] = edit_file('vsplit'),
+                ['ctrl-x'] = edit_file('split'),
+                ['ctrl-t'] = edit_file('tabnew'),
+            }
+        end,
     })
 end
 
@@ -218,6 +316,7 @@ local mappings = {
     { '<leader>ff', file_explorer, 'file explorer (cwd)' },
     { '<leader>fF', function() file_explorer('%:h') end, 'file explorer (cwd)' },
     { '<leader>fr', '<CMD>FzfLua oldfiles<CR>', 'recent files' },
+    { '<leader>fS', function() save_as('%:h') end, 'save as' },
     { '<leader>bb', '<CMD>FzfLua buffers<CR>', 'switch buffer' },
     { '<leader>pp', switch_project, 'switch project' },
     { '<leader>pf', '<CMD>FzfLua files<CR>', 'project files' },
