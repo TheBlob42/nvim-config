512b9ad365135852f2d102f1abfd3ccc1f4e301c
TheBlob42 <hessenmobbel@web.de>
2023-04-26 14:47:31 +0200

    fix: disable ts highlights for help & markdown

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,9 +3,11 @@ require('nvim-treesitter.configs').setup {
     highlight = {
         enable = true,
         disable = function(lang, buf)
-            -- disable highlighting for certain file types:
-            -- > help: https://github.com/nvim-treesitter/nvim-treesitter/pull/3555
-            if vim.tbl_contains({ 'help' }, lang) then
+            -- disable highlighting for certain file types
+            -- help     : https://github.com/nvim-treesitter/nvim-treesitter/pull/3555
+            -- vimdoc   : same as the "old" help filetype
+            -- markdown : bad performance on big files
+            if vim.tbl_contains({ 'help', 'vimdoc', 'markdown' }, lang) then
                 return true
             end
 
