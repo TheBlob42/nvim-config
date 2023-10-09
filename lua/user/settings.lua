4cf52e24aadcafd2631393bb4a87d39b46cfa02f
TheBlob42 <hessenmobbel@web.de>
2023-10-09 08:28:15 +0200

    fix: only show the "worst" diagnostic sign

--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -58,6 +58,33 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     desc = 'highlight yanked text',
 })
 
+-- ###################
+-- ### Diagnostics ###
+-- ###################
+
+-- only show the "worst" diagnostic sign (highest severity, see ':h diagnostic-handlers-example')
+local ns = vim.api.nvim_create_namespace('max_severity_only')
+local orig_signs_handler = vim.diagnostic.handlers.signs
+
+vim.diagnostic.handlers.signs = {
+    show = function(_, bufnr, _, opts)
+        local diagnostics = vim.diagnostic.get(bufnr)
+        local max_severity_per_line = {}
+        for _, d in pairs(diagnostics) do
+            local m = max_severity_per_line[d.lnum]
+            if not m or d.severity < m.severity then
+                max_severity_per_line[d.lnum] = d
+            end
+        end
+
+        local filtered_diagnostics = vim.tbl_values(max_severity_per_line)
+        orig_signs_handler.show(ns, bufnr, filtered_diagnostics, opts)
+    end,
+    hide = function(_, bufnr)
+        orig_signs_handler.hide(ns, bufnr)
+    end
+}
+
 -- define diagnostic icons and colors
 vim.fn.sign_define("DiagnosticSignError", {
     text = "",
