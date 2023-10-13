d516538f602f2ef1771dce168c9901141fdcbded
TheBlob42 <hessenmobbel@web.de>
2023-10-13 08:30:23 +0200

    chore: fix indentation

--- init.lua
+++ init.lua
@@ -15,14 +15,14 @@ end
 -- automatically install lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  }
+    vim.fn.system {
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    }
 end
 vim.opt.runtimepath:prepend(lazypath)
 
