c8e1010901540dde982c2365032d954be8df1912
TheBlob42 <hessenmobbel@web.de>
2023-10-27 13:43:45 +0200

    chore(fidget): update for max_lines

--- init.lua
+++ init.lua
@@ -207,7 +207,7 @@ require('lazy').setup {
     {
         -- show lsp progress
         'j-hui/fidget.nvim',
-        tag = 'legacy',
+        branch = 'legacy',
         config = function()
             require('fidget').setup {
                 text = { spinner = 'dots' }
