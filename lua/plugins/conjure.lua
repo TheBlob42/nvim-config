472a106fcbaa5ac6070eebcf9e8c35594c0adf96
TheBlob42 <hessenmobbel@web.de>
2023-10-27 19:11:31 +0200

    feat(conjure): mapping improvements
    - set ',' as the prefix for conjure mappings (faster access)
    - set correct which-key group label descriptions for conjure

--- lua/plugins/conjure.lua
+++ lua/plugins/conjure.lua
@@ -14,3 +14,30 @@ vim.api.nvim_create_autocmd('BufNewFile', {
     end,
     desc = 'disable diagnostics for conjure log buffer',
 })
+
+-- experimental mapping for now, lets see how (and if) this evolves
+vim.g['conjure#mapping#prefix'] = ','
+
+-- add custom prefix labels for which-key
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = my.lisps,
+    callback = function(opts)
+        if vim.api.nvim_buf_is_loaded(opts.buf) then
+            require('which-key').register({
+                name = 'Conjure',
+                ['c']  = { name = 'Connect' },
+                ['e']  = { name = 'Evaluate' },
+                ['ec'] = { name = 'Comment' },
+                ['g']  = { name = 'Get' },
+                ['l']  = { name = 'Logs' },
+                ['r']  = { name = 'Refresh' },
+                ['s']  = { name = 'Session' },
+                ['t']  = { name = 'Tests' },
+                ['v']  = { name = 'View' },
+            }, {
+                prefix = vim.g['conjure#mapping#prefix'],
+                buffer = opts.buf
+            })
+        end
+    end,
+})
