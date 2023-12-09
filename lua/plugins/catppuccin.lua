9ed4bf3db8d4188847d273050a087819b525b47c
TheBlob42 <hessenmobbel@web.de>
2023-12-09 16:17:03 +0100

    refactor(catppuccin): general refactor

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -9,32 +9,27 @@ require('catppuccin').setup {
     }
 }
 
-local dark = 'mocha'
-local light = 'latte'
+local function load_catppuccin()
+    vim.cmd.colorscheme('catppuccin')
 
-local current = light
-
-local function highlight_adaptions()
-    -- make debug line better visible
-    vim.cmd [[ hi! link debugPC CurSearch ]]
-    -- make cursorline more visible
-    vim.cmd [[ hi! link CursorLine ColorColumn ]]
+    -- some highlight adaptions
+    vim.cmd [[ hi! link debugPC CurSearch ]]      -- make debug line better visible
+    vim.cmd [[ hi! link CursorLine ColorColumn ]] -- make cursorline more visible
 end
 
 -- startup NVIM in dark mode in the evening hours
 local hour = os.date('*t').hour
 if hour > 18 or hour < 8 then
-    current = dark
+    vim.opt.background = "dark"
 else
-    current = light
+    vim.opt.background = "light"
 end
 
-vim.cmd('colorscheme catppuccin-' .. current)
-highlight_adaptions()
+load_catppuccin()
 
 -- toggle colorscheme between light and dark
 vim.api.nvim_create_user_command('ToggleDarkMode', function()
-    current = current == dark and light or dark
-    vim.cmd('colorscheme catppuccin-' .. current)
-    highlight_adaptions()
+    local background = vim.opt.background:get()
+    vim.opt.background = background == 'dark' and 'light' or 'dark'
+    load_catppuccin()
 end, {})
