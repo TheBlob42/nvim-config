6a3c92e4432527578f293cc13886e4637f869f29
TheBlob42 <hessenmobbel@web.de>
2023-12-22 11:05:29 +0100

    fix: make vim-repeat mappings work in terminal
    Additionally do some refactorings of the function and error messages

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -7,9 +7,8 @@ gitsigns.setup({
 })
 
 -- make jumps repeatable (with `vim-repeat`)
-local repeat_map = my.repeat_map
-repeat_map('<Plug>GitSignsNextHunk', '<CMD>lua require("gitsigns").next_hunk()<CR>')
-repeat_map('<Plug>GitSignsPrevHunk', '<CMD>lua require("gitsigns").prev_hunk()<CR>')
+my.repeat_map('<Plug>GitSignsNextHunk', '<CMD>lua require("gitsigns").next_hunk()<CR>')
+my.repeat_map('<Plug>GitSignsPrevHunk', '<CMD>lua require("gitsigns").prev_hunk()<CR>')
 
 vim.keymap.set('n', '<leader>gp', gitsigns.preview_hunk, { desc = 'preview git hunk' })
 vim.keymap.set('n', '<leader>gn', '<Plug>GitSignsNextHunk', { desc = 'next git hunk' })
--- lua/user/config.lua
+++ lua/user/config.lua
@@ -19,21 +19,44 @@ my.lisps = {
     "lisp" ,
 }
 
--- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
--- utility configuration functions
--- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- ~~~~~~~~~~~~~~~~~~~~~~
+-- repeatable keymappings
+-- ~~~~~~~~~~~~~~~~~~~~~~
 
----Create a repeatable <Plug> mapping via `vim-repeat`
+---Special handling for `vim-repeat` mappings in terminal buffers
+---
+---After the mapping execution the `b:changedtick` variable is increment twice (expectation would be to keep it's current value)
+---Why exactly this happens I have no idea, but it is probably one of the specialities and edge cases of terminal buffers in general
+---However this breaks the usage of `vim-repeat`, so we have to update the corresponding plugin variable to match the new `b:changedtick` value
+function my._repeat_map_terminal_check()
+    local buf = vim.api.nvim_get_current_buf()
+    if vim.api.nvim_get_option_value('buftype', { buf = buf }) == 'terminal' then
+        local count = 0
+        vim.api.nvim_create_autocmd('TextChanged', {
+            group = vim.api.nvim_create_augroup('RepeatTerminalGroup', {}),
+            buffer = buf,
+            callback = function()
+                count = count + 1
+                if count > 1 then
+                    vim.g.repeat_tick = vim.api.nvim_buf_get_changedtick(buf)
+                    return true -- remove the autocmd afterwards
+                end
+            end
+        })
+    end
+end
+
+---Create a repeatable <Plug> mapping via `vim-repeat` (for normal mode)
 ---@param plug string Name for the <Plug> mapping. Needs to start with "<Plug>"
 ---@param rhs string|function Either a mapping string or a function that should be executed
 function my.repeat_map(plug, rhs)
     local plugins = vim.tbl_map(function(p) return p[1] end, require('lazy').plugins())
     if plugins and not vim.tbl_contains(plugins, 'tpope/vim-repeat') then
-        print(debug.getinfo(2).source .. ' --> `vim-repeat` is not loaded!')
+        vim.api.nvim_echo({{ debug.getinfo(2).source .. ' --> `vim-repeat` is not loaded!', 'WarningMsg' }}, true, {})
         return
     end
 
-    if plug:sub(0, 6) ~= '<Plug>' then
+    if not vim.startswith(plug, '<Plug>') then
         vim.api.nvim_echo({{ 'Invalid <Plug> mapping: `plug` needs to start with "<Plug>"!', 'WarningMsg' }}, true, {})
         return
     end
@@ -43,9 +66,12 @@ function my.repeat_map(plug, rhs)
         command = function()
             rhs()
             vim.fn['repeat#set'](vim.api.nvim_replace_termcodes(plug, true, false, true), vim.v.count)
+            my._repeat_map_terminal_check()
         end
     elseif type(rhs) == 'string' then
-        command = rhs .. ':call repeat#set("\\' .. plug .. '", v:count)<CR>'
+        command = rhs
+            .. ':call repeat#set("\\' .. plug .. '", v:count)<CR>'
+            .. ':lua my._repeat_map_terminal_check()<CR>'
     else
         vim.api.nvim_echo({{ 'Wrong argument type: `rhs` needs to be a string or a function!', 'WarningMsg' }}, true, {})
         return
